7865b7813270bdd5472ef7c46ed59772d6e6401e
cindyli007 <findingnemo2000@gmail.com>
2019-08-25 10:33:25 -0700

    Merge branch 'master' of https://github.com/cindyLi007/Leetcode
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/LeetCode/com/google/math/XKindDeckCards.java
+++ src/main/LeetCode/com/google/math/XKindDeckCards.java
@@ -11,13 +11,14 @@ import java.util.Map;
  * All the cards in each group have the same integer.
  */
 public class XKindDeckCards {
-    // Time: O(N), Space: O(N)
-    public boolean hasGroupsSizeX(int[] deck) {
+    public static boolean hasGroupsSizeX_Wrong(int[] deck) {
         if (deck.length<2) return false;
         Map<Integer, Integer> numToCount = new HashMap<>();
         for (int i : deck) {
             numToCount.put(i, numToCount.getOrDefault(i, 0)+1);
         }
+        // the following code is wrong, because from the ex. input, smallestCount is 6, the other 2 values are 8 and 9
+        // each of them % smallestCount > 1 but 8 and 9 gcd is 1 so for this input it should return false;
         int smallestCount = deck.length;
         for (Integer value : numToCount.values()) {
             if (value == 1) return false;
@@ -28,5 +29,26 @@ public class XKindDeckCards {
             if (value%smallestCount==1) return false;
         }
         return true;
+    }
+
+    // Time: O(N), Space: O(N)
+    public static boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i : deck) map.put(i, map.getOrDefault(i, 0) + 1);
+        int res = 0;
+        for (Integer v : map.values()) {
+            res = gcd(res, v);
+        }
+        return res>1;
+    }
+
+    private static int gcd(int a, int b) {
+        return a==0 ? b : gcd(b%a, a);
+    }
+
+    public static void main(String... args) {
+        int[] deck = new int[]{0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1};
+        boolean res = hasGroupsSizeX(deck);
+        System.out.println(res);
     }
 }
