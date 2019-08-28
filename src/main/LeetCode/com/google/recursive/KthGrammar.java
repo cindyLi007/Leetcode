7a72a75624bcb9f52e9d813dd5b6c3ff94d1d503
cindyli007 <findingnemo2000@gmail.com>
2019-08-28 15:01:45 -0700

    Merge branch 'master' of https://github.com/cindyLi007/Leetcode
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/LeetCode/com/google/recursive/KthGrammar.java
+++ src/main/LeetCode/com/google/recursive/KthGrammar.java
@@ -2,25 +2,35 @@ package com.google.recursive;
 
 public class KthGrammar {
     public int kthGrammar(int N, int K) {
-        String s = Integer.toBinaryString(row(N));
-        return s.charAt(K-1) - '0';
+        return helper(--N, --K);
     }
 
-    private int row(int N) {
-        if (N==1) return 0;
-        int left = row(N-1);
-        int shift = 1<<(N-2);
-
-        int i = left << shift;
-        int i1 = left ^ 1;
-        return i | i1;
-
+    private int helper(int N, int K) {
+        if (N<=1) return K==0 ? 0 : 1;
+        int len = (int)Math.pow(2, N);
+        if (K<len/2) return helper(N-1, K);
+        return helper(N-1, K-len/2) ^ 1;
     }
 
     public static void main(String... args) {
         KthGrammar kthGrammar = new KthGrammar();
-        int res = kthGrammar.kthGrammar(2, 2);
-        System.out.println(res);
+        int N = 4, K = 5;
+        int res = kthGrammar.kthGrammar(N, K);
+        System.out.println(N + ", " + K + "-> " + res);
+        N=1; K=1;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
+        N=2; K=1;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
+        N=2; K=2;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
+        N=4; K=6;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
+        N=4; K=7;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
+        N=4; K=8;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
+        N=4; K=3;
+        System.out.println(N + ", " + K + "-> " + kthGrammar.kthGrammar(N, K));
 
     }
 }
