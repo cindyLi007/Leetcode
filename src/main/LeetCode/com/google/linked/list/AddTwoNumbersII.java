bf980377b65851171f2c559c4a385bdc523f3df4
cindyli007 <findingnemo2000@gmail.com>
2019-09-23 20:18:23 -0700

    Merge branch 'master' of https://github.com/cindyLi007/Leetcode
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/LeetCode/com/google/linked/list/AddTwoNumbersII.java
+++ src/main/LeetCode/com/google/linked/list/AddTwoNumbersII.java
@@ -7,7 +7,7 @@ import java.util.Stack;
  */
 public class AddTwoNumbersII {
   /**
-   * this can beat 69%.
+   * Time: O(N+M), Space: O(N+M)
    */
   public ListNode addTwoNumbers_stack(ListNode l1, ListNode l2) {
     Stack<ListNode> s1 = new Stack();
@@ -38,38 +38,42 @@ public class AddTwoNumbersII {
   }
 
   /**
-   * this can beat 17%, first calculate the diff of 2 linked list, recursively calculate the sum, use an offset to mark
-   * whether 2 lists intersect
+   * first calculate the diff of 2 linked list, recursively calculate the sum, use an offset to mark whether 2 lists intersect
+   * Time: O(N+M), Space: O(max(N, M)) for call stack
    */
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    int len1=len(l1), len2=len(l2);
-    ListNode res =  len1<len2 ? helper(l2, l1, len2-len1) : helper(l1, l2, len1-len2);
-    if (res.val<10) return res;
-    ListNode node = new ListNode(1);
-    res.val%=10;
-    node.next=res;
-    return node;
+    int len1 = length(l1), len2 = length(l2);
+    ListNode res=len1 > len2 ? helper(l1, l2, len1-len2) : helper(l2, l1, len2-len1);
+    if (res.val>9) {
+      ListNode l = new ListNode(1);
+      l.next = res;
+      res.val %= 10;
+      return l;
+    }
+    return res;
   }
 
-  private ListNode helper(ListNode l1, ListNode l2, int offset) {
-    if (l1==null) return null;
-    int sum = offset==0 ? l1.val+l2.val : l1.val;
-    ListNode next = offset == 0 ? helper(l1.next, l2.next, 0) : helper(l1.next, l2, offset-1);
-    if (next!=null && next.val>9) {
-      next.val%=10;
-      sum++;
+  private ListNode helper(ListNode longer, ListNode shorter, int offset) {
+    if (longer==null) return null;
+    ListNode next = helper(longer.next, offset==0 ? shorter.next : shorter,
+        offset==0 ? 0 : offset-1);
+    int sum = longer.val + (offset==0 ? shorter.val : 0);
+    if (next !=null && next.val>9) {
+      next.val %= 10;
+      sum += 1;
     }
     ListNode node = new ListNode(sum);
-    node.next=next;
+    node.next = next;
     return node;
   }
 
-  private int len(ListNode node) {
-    int count=0;
-    while(node!=null) {
+  private int length(ListNode l) {
+    int count = 0;
+    while (l!=null) {
       count++;
-      node=node.next;
+      l=l.next;
     }
     return count;
   }
+
 }
