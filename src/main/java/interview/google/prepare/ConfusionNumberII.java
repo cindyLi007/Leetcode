71dbb1b12617f869a1b1ae87732ff3a65455f6ae
cindyli007 <accountsall007@gmail.com>
2019-10-01 08:43:06 -0700

    Added more leetcode questions

--- src/main/java/interview/google/prepare/ConfusionNumberII.java
+++ src/main/java/interview/google/prepare/ConfusionNumberII.java
@@ -24,16 +24,15 @@ public class ConfusionNumberII {
   public int confusingNumberII(int N) {
 
     // first count how many numbers from 1 to N construct with "0, 1, 6, 8, 9"
-    int num = N+1; // must use N+1 because we want to include N
-    String s = String.valueOf(num);
-    List<Integer> list = new ArrayList<>();
-    for (int i=0; i<s.length(); i++) {
-      list.add(s.charAt(i) - '0');
-    }
+    // must use N+1 because we want to include N
+    String NS = String.valueOf(N+1);
+    // L stands for how many digits in N+1
+    int L = NS.length();
+    int[] digit = new int[L];
+    for (int i=0; i<L; i++) digit[i]=NS.charAt(i)-'0';
 
     // first compute number less than num and do not have most leading decimal, for ex. if num is 6822, we will count
     // from 1 to 999
-    int L = list.size(); // L stands from how many digits in num
     int count=0;
     // leading number must be from {1, 6, 8, 9} so it is 4 numbers, after that, it can be {0, 1, 6, 8, 9}, so it is 5
     for (int i=1; i<L; i++) {
@@ -44,10 +43,12 @@ public class ConfusionNumberII {
     // compute [M+1, N] for ex. if num is 7822, compute from [1000, 6822)
     // from the most significant digit, for each one choose small that digit key from {0, 1, 6, 8, 9}
     for (int i=0; i<L; i++) {
-      int d = list.get(i);
-      if (i==0 && d==1) continue; // if now is the most significant digit and it is 1, we could not choose 0, so skip
+      int d = digit[i];
+      // if now is the most significant digit and it is 1, we could not choose 0, so skip
+      if (i==0 && d==1) continue; 
       for (Integer key : map.keySet()) {
-        if (key.intValue() < d) { // we only choose key less than current d, for 6822, if now we hanle 8, we only check 60xx, 61xx, 66xx
+        // we only choose key less than current d, for 6822, if now we hanle 8, we only check 60xx, 61xx, 66xx
+        if (key.intValue() < d) {
           if (i==0 && key.intValue() == 0) continue;
           count += Math.pow(5, L-i-1);
         } else {
@@ -63,32 +64,31 @@ public class ConfusionNumberII {
 
   // Calculate all numbers which is Strobogrammatic number from [1, N]
   private int helper(int N) {
-    int len = String.valueOf(N).length();
     String NS = String.valueOf(N);
-    // if len==1, only can choose 1, 8
-    if (len==1) {
-      if (N<8) return 1;
-      return 2;
-    }
-    int count = 0;
-    List<String>[] list = (List<String>[])new ArrayList[len+1];
-    list[1] = new ArrayList<String>(){{add("0"); add("1"); add("8"); }};
-    // for len==1, 2 numbers 1, 8
-    count += 2;
-    list[0] = new ArrayList<String>(){{add("");}};
+    int L = NS.length();
+    // if L==1, only can choose 1, 8
+    if (L==1) return N < 8 ? 1 : 2;
 
-    // for each len i, use i-2 as the middle and add pair
-    for (int i=2; i<=len; i++) {
+    int count = 0;
+    List<String>[] list = (List<String>[])new ArrayList[L+1];
+    list[0] = new ArrayList<String>(){{add("");}};
+    list[1] = new ArrayList<String>(){{add("0"); add("1"); add("8"); }};
+    // for L==1, 2 numbers 1, 8
+    count += 2;
+
+    // for each L i, use i-2 as the middle and add pair
+    for (int i=2; i<=L; i++) {
       List<String> prev = list[i - 2];
       list[i] = new ArrayList<>();
+
       for (String s : prev) {
         for (Integer k : map.keySet()) {
           // could not append 0 as the most significant digit
-          if (k==0 && i==len) continue;
+          if (k==0 && i==L) continue;
           String res = k.toString() + s + map.get(k).toString();
-          if (i==len && res.compareTo(NS) > 0) break;
+          if (i==L && res.compareTo(NS) > 0) break;
           list[i].add(res);
-          // if i<len we can add for ex. 080 to list, so later it can be 80808, but we should not count it
+          // if i<L we can add for ex. 080 to list, so later it can be 80808, but we should not count it
           if (!res.startsWith("0")) count++;
         }
       }
