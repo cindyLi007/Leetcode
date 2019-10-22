f4127e8fd89db30e5c653bca777e8085d6ced315
cindyli007 <findingnemo2000@gmail.com>
2019-10-21 20:37:06 -0700

    Merge branch 'master' of https://github.com/cindyLi007/Leetcode
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/LeetCode/com/google/dp/GuessNumHighOrLowerII.java
+++ src/main/LeetCode/com/google/dp/GuessNumHighOrLowerII.java
@@ -1,8 +1,9 @@
 package com.google.dp;
 
-import java.util.Arrays;
-
+// leetcode 375. Guess Number Higher or Lower II
+// MiniMax
 public class GuessNumHighOrLowerII {
+    // Time: O(N^3), Space: O(N^2)
     public int getMoneyAmount(int n) {
         int[][] dp = new int[n+1][n+1];
         return get(1, n, dp);
@@ -15,6 +16,8 @@ public class GuessNumHighOrLowerII {
         int res = Integer.MAX_VALUE;
         if (dp[start][end]==0) {
             for (int x=start+1; x<end; x++) {
+                // for 每次的每一种选择都找后面最坏的可能(minimize result)和它combine(这相当于假设对手总能给我对我最不利的情况）
+                // 然后从这所有的可能中选一种最好的选择(maximize result)
                 int cost = x + Math.max(get(start, x-1, dp), get(x+1, end, dp));
                 res = Math.min(res, cost);
             }
